2f158b5cb44415dae692cdfa4f537fb8183c878f
Rodrigo Gomez <43794366+RodrigoGomez14@users.noreply.github.com>
2023-01-27 22:27:10 -0300

    0.4.9.1

--- src/Pages/CuentasBancarias.jsx
+++ src/Pages/CuentasBancarias.jsx
@@ -6,7 +6,7 @@ import {AttachMoney,PersonAdd} from '@material-ui/icons'
 import {Link} from 'react-router-dom'
 import {formatMoney} from '../utilities'
 import {content} from './styles/styles'
-import { CardCliente } from '../components/Clientes/CardCliente'
+import CardCuentaBancaria from '../components/Cuentas-Bancarias/CardCuentaBancaria'
 import Empty from '../images/Empty.png'
 // COMPONENT
 const CuentasBancarias=(props)=>{
@@ -18,8 +18,35 @@ const CuentasBancarias=(props)=>{
             {/* CONTENT */}
             <Paper className={classes.content}>
                 <Grid container spacing={4}>
+                    {/* SEARCH BAR */}
+                    <Grid container item xs={12} justify='center' alignItems='center'>
+                        <Grid item>
+                            <Link 
+                                to='/Nuevo-Cliente'>
+                                <IconButton>
+                                    <PersonAdd/>
+                                </IconButton>
+                            </Link>
+                        </Grid>
+                    </Grid>
+
                     {/* CLIENT LIST */}
-                    
+                    <Grid container item xs={12} justify='center' alignItems='center' spacing={2} >
+                        {props.CuentasBancarias?
+                            Object.keys(props.CuentasBancarias).map(key=>(
+                                <CardCuentaBancaria cuenta={key}/>
+                            ))
+                            :
+                            <>
+                                <Grid item>
+                                    <img src={Empty} alt="" height='600px'/>
+                                </Grid>
+                                <Grid container item xs={12} justify='center'>
+                                    <Typography variant='h4'>No hay Cuentas Bancarias</Typography>
+                                </Grid>
+                            </>
+                        }
+                    </Grid>
                 </Grid>
             </Paper>
         </Layout>
---
+++ src/components/Cuentas-Bancarias/CardCuentaBancaria.jsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import {CardHeader,Paper,ListItem,Card,CardContent,Typography,TextField,List,Grid,Chip,IconButton,Link as LinkComponent} from '@material-ui/core'
+import {content} from '../../Pages/styles/styles'
+// COMPONENT
+const CardCuentaBancaria=({cuenta})=>{
+    const classes = content()
+    
+    return(
+        <Grid container item xs={12} justify='center'>
+            <Card>
+                <CardHeader
+                    title={cuenta}
+                />
+            </Card>
+        </Grid>
+    )
+}
+export default CardCuentaBancaria
